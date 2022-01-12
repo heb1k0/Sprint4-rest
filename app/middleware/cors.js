ba0dc3a8eaece875b84789b4ece0d31d2172dc97
Alejandro <alejandrozuriguel@gmail.com>
2022-01-12 20:52:17 +0100

    Delete app directory

--- app/middleware/cors.js
+++
@@ -1,4 +0,0 @@
-exports.cacheController = (req, res, next) => {
-    res.setHeader('Cache-control', 'no-cache');
-    next();
-}
--- app/routes/upload.js
+++
@@ -1,9 +0,0 @@
-const controllers = require('../controllers/FileController');
-const express = require('express');
-const router=  express.Router();
-
-
-router.post('/upload', controllers.uploadFile);
-
-
-module.exports = router;
